d0173881b45c0e435d133726cbc510e80ceaf4f3
Akshay Chandel <139372635+akshaychandel1@users.noreply.github.com>
2026-04-17 16:02:37 +0530

    Simple Api Created

---
+++ simple_api.js
@@ -0,0 +1,29 @@
+const http = require('http');
+
+const userData = [
+    {
+        name: 'John Doe',
+        age: 30,
+        email: 'john.doe@example.com'
+    } ,
+    {
+        name: 'Jane Smith',
+        age: 25,
+        email: 'jane.smith@example.com'
+    } ,
+    {
+        name: 'Alice Johnson',
+        age: 28,
+        email: 'alice.johnson@example.com'
+    }
+
+];
+
+http.createServer((req, res) =>{
+
+res.setHeader('Content-Type', 'application/json');
+res.write(JSON.stringify(userData))
+res.end();
+
+
+}).listen(3100);
