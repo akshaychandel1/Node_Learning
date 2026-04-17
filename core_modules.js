4b9512a50ad3cadfecb9443f87a3fc6c71f38160
Akshay Chandel <139372635+akshaychandel1@users.noreply.github.com>
2026-04-17 12:29:37 +0530

    first commit

---
+++ core_modules.js
@@ -0,0 +1,8 @@
+const { log } = require('console');
+const fs = require('fs');   // importing of core module
+const os = require('os');
+
+// fs.writeFileSync('dummy.txt',"Learning modules in node js");  // using of core module
+
+console.log(os.platform());
+console.log(os.hostname());
