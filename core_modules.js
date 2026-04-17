563085da47198466ff398a0ac174af595f41550c
Akshay Chandel <139372635+akshaychandel1@users.noreply.github.com>
2026-04-17 12:55:18 +0530

    1st lesson completed

--- core_modules.js
+++ core_modules.js
@@ -1,8 +1,39 @@
-const { log } = require('console');
-const fs = require('fs');   // importing of core module
-const os = require('os');
+// const { log } = require('console');
+// const fs = require('fs');   // importing of core module
+// const os = require('os');
+const {log, warn} = require('console')
+const print = require('console')
 
-// fs.writeFileSync('dummy.txt',"Learning modules in node js");  // using of core module
+// // fs.writeFileSync('dummy.txt',"Learning modules in node js");  // using of core module
 
-console.log(os.platform());
-console.log(os.hostname());
+// console.log(os.platform());
+// console.log(os.hostname());
+// console.log(os.arch());
+// console.log(os.cpus());
+// console.log(os.freemem());
+// console.log(os.totalmem());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("console.log() -> this is a global object (object b o hoti h jisse kabhi bhi improt krne ki jarurat nhi padti h  ");
+
+// console.log(process.cwd());
+// console.log(process.pid);
+// console.log("hello ");
+// log("hello ");
+// print.log("hello ");
+
+warn("this is a warning message");
